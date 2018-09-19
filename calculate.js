7de0ba9c07dbd528cee3251e22010457e4178057
chrismgonzalez <cmgonza89@gmail.com>
2018-09-18 20:12:37 -0500

    added button logic and calc object

--- calculate.js
+++ calculate.js
@@ -1,42 +1,20 @@
-//listen for keypresses & determine type of key pressed
 
-const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculator__keys');
+//initialize calculator object
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitForSecondOperand: false,
+  operator: null,
+};
+
+//button click handler
+const keyClick = document.querySelector('.calculator__keys');
+
+keyClick.addEventListener('click', function() {
+  alert("Button clicked!");
+});
+
+//update calculator screen display
 
 
-//listen for keypress
-keys.addEventListener('click', e => {
-  if( e.target.matches('button')) {
-    //do something
-  }
-})
 
-const key = e.target;
-const action = key.dataset.action;
-
-//determine type of key pressed
-
-if(!action) {
-  console.log('number key!')
-}
-
-if(
-  action === 'add' ||
-  action === 'subtract' ||
-  action === 'multiply' ||
-  action === 'divide'
-) {
-  console.log('operator key!')
-}
-
-if (action === 'decimal') {
-  console.log('decimal key!')
-}
-
-if (action === 'clear') {
-  console.log('clear key')
-}
-
-if (action === 'calculate') {
-  console.log('equal key!')
-}
